4de1f08aa95a5d73209b39e76653eaa6f5257da3
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2026-08-03 08:43:11 -0300

    save

--- Averiguar_kg.py
+++ Averiguar_kg.py
@@ -7,7 +7,7 @@ def carregar_planilhas():
     
     print("Carregando arquivo Excel - aba FEC_PQ...")
     margem_df = pd.read_excel(
-        r"C:\Users\DELL\Downloads\260721_MRG.xlsx",
+        r"C:\Users\DELL\Downloads\260730_MRG.xlsx",
         sheet_name="FEC_PQ",
         header=9,  # A10 corresponde à linha 10 (0-index: linha 9)
         skiprows=0
